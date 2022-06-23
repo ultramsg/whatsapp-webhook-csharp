e018c3437f7dca349a51a436438580a425ec7ec3
Ultramsg <95187366+ultramsg@users.noreply.github.com>
2022-06-23 05:35:01 +0300

    Add files via upload

---
+++ Controllers/UltraMessageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace WebHookExample.Properties
+{
+    [Route("api/")]
+    [ApiController]
+    public class UltraMessageController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] HookData data)
+        {
+            Console.WriteLine(JsonSerializer.Serialize(data));
+            return Ok(data);
+        }
+    }
+}
